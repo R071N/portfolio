3c4c4a3fcea48979432bf987f39c2a295cd86c87
Александр Ролин <drawpf@mail.ru>
2023-05-03 17:13:05 +0300

    add works 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import AboutView from "@/views/AboutView.vue";
 import SkillsView from "@/views/SkillsView.vue";
 import WorksView from "@/views/WorksView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
